df8d1fa232a7fd87a32beced57545ae9c1f20b81
Lvova Alina <lvova.aline@gmail.com>
2023-06-06 17:01:54 +0400

    fix: change localhost

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,5 @@
 const { JWT_SECRET_KEY = 'mAlina-SecretKey' } = process.env;
-const { DB_ADDRESS = 'mongodb://51.250.20.120:27017/mestodb' } = process.env;
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
   JWT_SECRET_KEY,
