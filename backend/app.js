a9ac80ba800274061f10830eed77bbf022c6a127
Lvova Alina <lvova.aline@gmail.com>
2023-06-09 23:56:42 +0400

    fearute: add cors

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3001' }));
 
 // теперь клиент имеет доступ только к публичным файлам
 app.use(express.static(path.join(__dirname, 'public')));
